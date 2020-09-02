48fb8fa93706b43de4f1e726c19414fb158f6247
Lucas Vegi <lucasvegi@gmail.com>
2020-09-02 12:00:01 -0300

    Teste comentário

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/CadastrarActivity.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/CadastrarActivity.java
@@ -18,6 +18,7 @@ public class CadastrarActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar);
+        //teste
     }
 
     public void clickVoltar(View v){
